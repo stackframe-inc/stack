6ae264ae848b0d560009308b74b56ce49610800a
TheCactusBlue <thecactusblue@gmail.com>
2025-02-27 11:51:06 -0800

    add some additional logic for issuing api key

--- apps/e2e/tests/js/js-helpers.ts
+++ apps/e2e/tests/js/js-helpers.ts
@@ -1,11 +1,19 @@
 import { StackAdminApp } from '@stackframe/js';
 
-export function scaffoldProject() {
-  const stk = new StackAdminApp({
+export async function scaffoldProject() {
+  const adminApp = new StackAdminApp({
     projectId: "internal",
     publishableClientKey: "your-publishable-client-key-from-dashboard",
     secretServerKey: "your-secret-server-key-from-dashboard",
     tokenStore: "memory",
     superSecretAdminKey: "ADMIN_KEY"
+  });
+
+  await adminApp.createApiKey({
+    hasPublishableClientKey: true,
+    hasSecretServerKey: true,
+    hasSuperSecretAdminKey: false,
+    description: "Test API Key Please Ignore",
+    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
   });
 }
