423e80b56132f398e0554c5309cc2ba78d6522a5
TheCactusBlue <thecactusblue@gmail.com>
2025-02-27 14:24:35 -0800

    more

--- apps/e2e/tests/js/general.test.ts
+++ apps/e2e/tests/js/general.test.ts
@@ -1,5 +1,7 @@
 import { it } from "../helpers";
+import { scaffoldProject } from "./js-helpers";
 
-it("should pass", async ({ expect }) => {
-  expect(1 + 2).toBe(3);
+it("should scaffold the project", async ({ expect }) => {
+  const { project } = await scaffoldProject();
+  expect(project).toMatchInlineSnapshot();
 });
--- apps/e2e/tests/js/js-helpers.ts
+++ apps/e2e/tests/js/js-helpers.ts
@@ -1,19 +1,40 @@
 import { StackAdminApp } from '@stackframe/js';
+import { wait } from '@stackframe/stack-shared/dist/utils/promises';
+import { STACK_BACKEND_BASE_URL, STACK_INTERNAL_PROJECT_ADMIN_KEY, STACK_INTERNAL_PROJECT_CLIENT_KEY, STACK_INTERNAL_PROJECT_SERVER_KEY } from '../helpers';
 
 export async function scaffoldProject() {
-  const adminApp = new StackAdminApp({
-    projectId: "internal",
-    publishableClientKey: "your-publishable-client-key-from-dashboard",
-    secretServerKey: "your-secret-server-key-from-dashboard",
+  const internalApp = new StackAdminApp({
+    projectId: 'internal',
+    baseUrl: STACK_BACKEND_BASE_URL,
+    publishableClientKey: STACK_INTERNAL_PROJECT_CLIENT_KEY,
+    secretServerKey: STACK_INTERNAL_PROJECT_SERVER_KEY,
+    superSecretAdminKey: STACK_INTERNAL_PROJECT_ADMIN_KEY,
     tokenStore: "memory",
-    superSecretAdminKey: "ADMIN_KEY"
   });
 
-  await adminApp.createApiKey({
-    hasPublishableClientKey: true,
-    hasSecretServerKey: true,
-    hasSuperSecretAdminKey: false,
-    description: "Test API Key Please Ignore",
-    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
+  const fakeEmail = `${crypto.randomUUID()}@stack-js-test.example.com`;
+
+  await internalApp.signUpWithCredential({
+    email: fakeEmail,
+    password: "password",
   });
+  await wait(2000);
+  const user = await internalApp.getUser({
+    or: 'throw',
+  });
+
+  const project = await user.createProject({
+    displayName: "Test Project",
+    config: {
+      signUpEnabled: true,
+      credentialEnabled: true,
+      magicLinkEnabled: true,
+      passkeyEnabled: true,
+    },
+  });
+
+  return {
+    project,
+    user,
+  };
 }
